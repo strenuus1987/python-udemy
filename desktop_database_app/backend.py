9441a559e73fb6071248422b71aa8bea485318ae
Andrey Maznov <strenuus1987@gmail.com>
2018-01-22 20:49:04 +0200

    OOP refactoring was made

--- desktop_database_app/backend.py
+++ desktop_database_app/backend.py
@@ -1,47 +1,37 @@
 import sqlite3
 
-def create_table():
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
-    connect.commit()
-    connect.close()
+class Database:
 
-def view_all():
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    cursor.execute("SELECT * FROM books")
-    res = cursor.fetchall()
-    connect.close()
-    return res
+    def __init__(self, db):
+        self.connect = sqlite3.connect(db)
+        self.cursor = self.connect.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        self.commit()
 
-def insert(title, author, year, isbn):
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    cursor.execute("INSERT INTO books VALUES(NULL, ?, ?, ?, ?)",(title, author, year, isbn))
-    connect.commit()
-    connect.close()
+    def view_all(self):
+        self.cursor.execute("SELECT * FROM books")
+        res = self.cursor.fetchall()
+        return res
 
-def search(title="", author="", year="", isbn=""):
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    cursor.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
-    res = cursor.fetchall()
-    connect.close()
-    return res
+    def insert(self, title, author, year, isbn):
+        self.cursor.execute("INSERT INTO books VALUES(NULL, ?, ?, ?, ?)",(title, author, year, isbn))
+        self.commit()
 
-def delete(id):
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    cursor.execute("DELETE FROM books WHERE id=?", (id,))
-    connect.commit()
-    connect.close()
+    def search(self, title="", author="", year="", isbn=""):
+        self.cursor.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+        res = self.cursor.fetchall()
+        return res
 
-def update(id, title, author, year, isbn):
-    connect = sqlite3.connect('database.db')
-    cursor = connect.cursor()
-    cursor.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
-    connect.commit()
-    connect.close()
+    def delete(self, id):
+        self.cursor.execute("DELETE FROM books WHERE id=?", (id,))
+        self.commit()
 
-create_table()
+    def update(self, id, title, author, year, isbn):
+        self.cursor.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+        self.commit()
+
+    def commit(self):
+        self.connect.commit()
+
+    def __del__(self):
+        self.connect.close()
--- desktop_database_app/frontend.py
+++ desktop_database_app/frontend.py
@@ -12,104 +12,114 @@ Delete entry
 Close
 """
 from tkinter import *
-import backend
+from backend import Database
 
-def get_selected_row(event):
-    try:
-        global selected_tuple
-        index = list_box.curselection()[0]
-        selected_tuple = list_box.get(index)
-        e_title.delete(0, END)
-        e_title.insert(END, selected_tuple[1])
-        e_author.delete(0, END)
-        e_author.insert(END, selected_tuple[2])
-        e_year.delete(0, END)
-        e_year.insert(END, selected_tuple[3])
-        e_isbn.delete(0, END)
-        e_isbn.insert(END, selected_tuple[4])
-    except IndexError:
-        pass
+class MainWindow():
 
-def view_command():
-    list_box.delete(0, END)
-    for row in backend.view_all():
-        list_box.insert(END, row)
+    def __init__(self, database):
 
-def search_command():
-    list_box.delete(0, END)
-    for row in backend.search(var_title.get(), var_author.get(), var_year.get(), var_isbn.get()):
-        list_box.insert(END, row)
+        self.database = database
 
-def add_command():
-    backend.insert(var_title.get(), var_author.get(), var_year.get(), var_isbn.get())
-    view_command()
+        self.window = Tk()
+        self.window.title("Books database")
 
-def delete_command():
-    backend.delete(selected_tuple[0])
-    view_command()
+        self.l_title=Label(self.window, text="Title")
+        self.l_title.grid(row=0, column=0)
 
-def update_command():
-    backend.update(selected_tuple[0], var_title.get(), var_author.get(), var_year.get(), var_isbn.get())
-    view_command()
+        self.l_year=Label(self.window, text="Year")
+        self.l_year.grid(row=1, column=0)
 
-window = Tk()
-window.title("Books database")
+        self.l_author=Label(self.window, text="Author")
+        self.l_author.grid(row=0, column=2)
 
-l_title=Label(window, text="Title")
-l_title.grid(row=0, column=0)
+        self.l_isbn=Label(self.window, text="ISBN")
+        self.l_isbn.grid(row=1, column=2)
 
-l_year=Label(window, text="Year")
-l_year.grid(row=1, column=0)
+        self.var_title = StringVar()
+        self.e_title = Entry(self.window, textvariable=self.var_title)
+        self.e_title.grid(row=0, column=1)
 
-l_author=Label(window, text="Author")
-l_author.grid(row=0, column=2)
+        self.var_year = StringVar()
+        self.e_year = Entry(self.window, textvariable=self.var_year)
+        self.e_year.grid(row=1, column=1)
 
-l_isbn=Label(window, text="ISBN")
-l_isbn.grid(row=1, column=2)
+        self.var_author = StringVar()
+        self.e_author = Entry(self.window, textvariable=self.var_author)
+        self.e_author.grid(row=0, column=3)
 
-var_title = StringVar()
-e_title = Entry(window, textvariable=var_title)
-e_title.grid(row=0, column=1)
+        self.var_isbn = StringVar()
+        self.e_isbn = Entry(self.window, textvariable=self.var_isbn)
+        self.e_isbn.grid(row=1, column=3)
 
-var_year = StringVar()
-e_year = Entry(window, textvariable=var_year)
-e_year.grid(row=1, column=1)
+        self.list_box = Listbox(self.window, height=8, width=35)
+        self.list_box.grid(row=2, column=0, rowspan=6, columnspan=2)
 
-var_author = StringVar()
-e_author = Entry(window, textvariable=var_author)
-e_author.grid(row=0, column=3)
+        self.scrollbar = Scrollbar(self.window)
+        self.scrollbar.grid(row=2, column=2, rowspan=6, sticky='ns')
 
-var_isbn = StringVar()
-e_isbn = Entry(window, textvariable=var_isbn)
-e_isbn.grid(row=1, column=3)
+        self.list_box.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.configure(command=self.list_box.yview)
 
-list_box = Listbox(window, height=8, width=35)
-list_box.grid(row=2, column=0, rowspan=6, columnspan=2)
+        self.list_box.bind('<<ListboxSelect>>', self.get_selected_row)
 
-scrollbar = Scrollbar(window)
-scrollbar.grid(row=2, column=2, rowspan=6, sticky='ns')
+        self.button_view_all = Button(self.window, text="View all", width=12, command=self.view_command)
+        self.button_view_all.grid(row=2, column=3)
 
-list_box.configure(yscrollcommand=scrollbar.set)
-scrollbar.configure(command=list_box.yview)
+        self.button_search = Button(self.window, text="Search entry", width=12, command=self.search_command)
+        self.button_search.grid(row=3, column=3)
 
-list_box.bind('<<ListboxSelect>>', get_selected_row)
+        self.button_add = Button(self.window, text="Add entry", width=12, command=self.add_command)
+        self.button_add.grid(row=4, column=3)
 
-button_view_all = Button(window, text="View all", width=12, command=view_command)
-button_view_all.grid(row=2, column=3)
+        self.button_update = Button(self.window, text="Update", width=12, command=self.update_command)
+        self.button_update.grid(row=5, column=3)
 
-button_search = Button(window, text="Search entry", width=12, command=search_command)
-button_search.grid(row=3, column=3)
+        self.button_delete = Button(self.window, text="Delete", width=12, command=self.delete_command)
+        self.button_delete.grid(row=6, column=3)
 
-button_add = Button(window, text="Add entry", width=12, command=add_command)
-button_add.grid(row=4, column=3)
+        self.button_close = Button(self.window, text="Close", width=12, command=self.window.destroy)
+        self.button_close.grid(row=7, column=3)
 
-button_update = Button(window, text="Update", width=12, command=update_command)
-button_update.grid(row=5, column=3)
+        self.window.mainloop()
 
-button_delete = Button(window, text="Delete", width=12, command=delete_command)
-button_delete.grid(row=6, column=3)
+    def get_selected_row(self, event):
+        try:
+            index = self.list_box.curselection()[0]
+            self.selected_tuple = self.list_box.get(index)
+            self.e_title.delete(0, END)
+            self.e_title.insert(END, self.selected_tuple[1])
+            self.e_author.delete(0, END)
+            self.e_author.insert(END, self.selected_tuple[2])
+            self.e_year.delete(0, END)
+            self.e_year.insert(END, self.selected_tuple[3])
+            self.e_isbn.delete(0, END)
+            self.e_isbn.insert(END, self.selected_tuple[4])
+        except IndexError:
+            pass
 
-button_close = Button(window, text="Close", width=12, command=window.destroy)
-button_close.grid(row=7, column=3)
+    def clear_list(self):
+        self.list_box.delete(0, END)
 
-window.mainloop()
+    def view_command(self):
+        self.clear_list()
+        for row in self.database.view_all():
+            self.list_box.insert(END, row)
+
+    def search_command(self):
+        self.clear_list()
+        for row in self.database.search(self.var_title.get(), self.var_author.get(), self.var_year.get(), self.var_isbn.get()):
+            self.list_box.insert(END, row)
+
+    def add_command(self):
+        self.database.insert(self.var_title.get(), self.var_author.get(), self.var_year.get(), self.var_isbn.get())
+        self.view_command()
+
+    def delete_command(self):
+        self.database.delete(self.selected_tuple[0])
+        self.view_command()
+
+    def update_command(self):
+        self.database.update(self.selected_tuple[0], self.var_title.get(), self.var_author.get(), self.var_year.get(), self.var_isbn.get())
+        self.view_command()
+
+window = MainWindow(Database('database.db'))
