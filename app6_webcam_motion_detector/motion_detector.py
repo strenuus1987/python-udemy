59975776238821f46f379574d1ec01e8838df166
Andrey Maznov <strenuus1987@gmail.com>
2018-02-02 12:38:16 +0200

    result of motion detection via bokeh library added

--- app6_webcam_motion_detector/motion_detector.py
+++ app6_webcam_motion_detector/motion_detector.py
@@ -38,6 +38,9 @@ while True:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3) #draw this rectangle on the frame
 
     status_list.append(status)
+
+    status_list = status_list[-2];
+
     if status_list[-1]==1 and status_list[-2]==0:
         times.append(datetime.now())
     if status_list[-1]==0 and status_list[-2]==1:
